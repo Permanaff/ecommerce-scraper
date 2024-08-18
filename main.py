6a7a4c1594e70419c709833bdc9251985abbc857
Permanaff <138554853+Permanaff@users.noreply.github.com>
2024-08-18 22:22:11 +0700

    updatem main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from time import sleep
 import pandas as pd
+from datetime import datetime
+
 
 driver=webdriver.Chrome()
 driver.set_page_load_timeout(60)
@@ -15,14 +17,14 @@ driver.maximize_window()
 sleep(5)
 search=driver.find_element(By.XPATH,"""//*[@id="header-main-wrapper"]/div[2]/div[2]/div/div/div/div/input""")
 search.clear()
-search.send_keys('sandisk')
+search.send_keys('laptop lenovo')
 
 search.send_keys(Keys.ENTER)
 sleep(5)
 for i in range(8):
     driver.execute_script("window.scrollBy(0,700)","")
     sleep(2)
-sleep(10)
+sleep(5)
 container_element = driver.find_element(By.CLASS_NAME,'css\\-rjanld')
 driver.execute_script("arguments[0].scrollIntoView();", container_element)
 
@@ -33,13 +35,8 @@ pagination = WebDriverWait(driver, 10).until(
 pages = pagination.find_element(By.XPATH, './/ul[contains(@class, "css-1ni9y5x-unf-pagination-items")]')
 page_items = pages.find_elements(By.TAG_NAME, 'li')
 last_page = int(page_items[-2].text.replace('.', ''))
-# if last_page >= 20 : 
-#     last_page = 20
-
-last_page = 5
-
-print(last_page)
-
+if last_page >= 10 : 
+    last_page = 10
 
 # sleep(10)
 title_product = []
@@ -47,7 +44,6 @@ product_price = []
 seller_product = []
 product_sell = []
 product_image = []
-print('a')
 current_page = 1
 
 
@@ -60,19 +56,19 @@ while current_page <= last_page:
             title = product.find_element(By.CLASS_NAME, 'OWkG6oHwAppMn1hIBsC3pQ\\=\\=').text
         except NoSuchElementException:
             title = None
+
+        try : 
+            title = title.replace(",", " ")
+        except NoSuchElementException:
+            title = title
         title_product.append(title)
 
-        # price_elemen = product.find_element(By.CLASS_NAME, 'ELhJqP-Bfiud3i5eBR8NWg==')
-
         try:
-            # Mencoba menemukan elemen pertama
             price = product.find_element(By.CLASS_NAME, '_8cR53N0JqdRc\\+mQCckhS0g\\=\\=').text
         except NoSuchElementException:
             try:
-                # Jika elemen pertama tidak ditemukan, coba elemen alternatif
                 price = product.find_element(By.CLASS_NAME, 'gJHohDcsji\\+TjH4Kkc9LEw\\=\\=').text
             except NoSuchElementException:
-                # Tangani kasus jika elemen juga tidak ditemukan
                 price = None
         product_price.append(price)
 
@@ -82,6 +78,7 @@ while current_page <= last_page:
             sell = product.find_element(By.CLASS_NAME, 'eLOomHl6J3IWAcdRU8M08A\\=\\=').text
         except NoSuchElementException:
             sell = None
+
         product_sell.append(sell)
 
         try:
@@ -103,23 +100,36 @@ while current_page <= last_page:
             seller = None
 
         seller_product.append(seller)
-        
 
-        driver.execute_script(f"console.log('{title}');")
+        
         print(f"{title}\n{price}\n{sell}\n{image}\n--------------------")
 
+        try : 
+            escaped_title = title.replace("'", "\\'").replace('"', '\\"')
+        except NoSuchElementException:
+            escaped_title = title
+
+        # if "'" in title or '"' in title:
+        #     escaped_title = title.replace("'", "\\'").replace('"', '\\"')
+        
+        driver.execute_script(f'console.log(`{escaped_title}`);')
+
+    driver.execute_script(f'console.log(">>>>> Data Terambil :  {len(title_product)} <<<<<");')
     current_page = current_page + 1
     try:
         next_page = driver.find_element(By.XPATH, './/button[contains(@class, "css-16uzo3v-unf-pagination-item")][@aria-label="Laman berikutnya"]')
         if next_page.is_enabled():
             next_page.click()
+            driver.execute_script(f'console.log(">>>>> HALAMAN {current_page} <<<<<");')
         else:
             break
     except NoSuchElementException:
         break
 
 
+time = datetime.now().strftime("%y%m%d%H%M%S")
+
 df=pd.DataFrame({"title":title_product, 'harga' : product_price, 'terjual' : product_sell, 'seller' : seller_product})
-df.to_csv("sandisk_product_tokopedia.csv",index=False)
+df.to_csv(f"product_{time}.csv",index=False)
 assert "No results found." not in driver.page_source
 driver.close()
